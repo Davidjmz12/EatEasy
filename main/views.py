b78b8b8196e9b24eb1adec5e17a9222ffee57fce
DavidTizne <davidtizne@gmail.com>
2023-10-10 17:44:43 +0200

    Corrección de la equivocación menu-restaurante

--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
 
+class Restaurante:
+  def __init__(self, name):
+    self.name = name
+
+
+Restaurantes = [Restaurante("Restaurante 1"),
+                Restaurante("Restaurante 2"),
+                Restaurante("Restaurante 3"),
+                Restaurante("Restaurante 4")]
+
+
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
 
 
 def search(request):
-    return render(request, "main/search.html")
+    return render(request, "main/search.html", {
+        "restaurants": Restaurantes
+    })
