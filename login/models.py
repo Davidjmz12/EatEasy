6703d8df3eaf1eec951e67febafa57855333ee49
DavidTizne <davidtizne@gmail.com>
2023-10-11 17:09:12 +0200

    Arreglado el Scroll y el desbordamiento de las descripciones

--- login/models.py
+++ login/models.py
@@ -86,14 +86,14 @@ class Dish(models.Model):
     name = models.CharField(max_length=20)
 
 
-class Rating(models.Model):
+def validate_rate(value):
+    if 0 <= value <= 5:
+        return value
+    else:
+        raise ValidationError("The rating is a number between 1 and 5")
 
-    @staticmethod
-    def validate_rate(value):
-        if 0 <= value <= 5:
-            return value
-        else:
-            raise ValidationError("The rating is a number between 1 and 5")
+
+class Rating(models.Model):
 
     rate = models.IntegerField(validators=[validate_rate])
     comment = models.CharField(max_length=300)
