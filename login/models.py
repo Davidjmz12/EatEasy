a2407294a65b906a051649c905b3011dcdc39b7a
David Jimenez <davidjmzom1@gmail.com>
2023-10-22 14:29:16 +0200

    First Version

--- login/models.py
+++ login/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     base_role = Role.ADMIN
 
     role = models.CharField(max_length=10, choices=Role.choices)
-    avatar = models.ImageField()
+    avatar = models.ImageField(blank=True)
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -44,12 +44,13 @@ class RestaurantManager(BaseUserManager):
 class Restaurant(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="Restaurants")
-
     # New attributes for the restaurant.
     web_page = models.URLField(blank=True)
     precise_location = models.CharField(max_length=100)
     city = models.CharField(max_length=20)
     phone_number = models.IntegerField()
+    description = models.CharField(max_length=200)
+    rest_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.user.username
@@ -59,18 +60,3 @@ class DeliveryPage(models.Model):
     name = models.CharField(max_length=20)
     url = models.URLField()
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="delivery_urls")
-
-
-# class Restaurant(User):
-#     base_role = User.Role.RESTAURANT
-#
-#     # New attributes for the restaurant.
-#     web_page = models.URLField(blank=True)
-#     precise_location = models.CharField(max_length=100)
-#     city = models.CharField(max_length=20)
-#     phone_number = models.IntegerField()
-#
-#     restaurants = RestaurantManager()
-#
-#     class Meta:
-#         proxy = True
