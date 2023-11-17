1f852377627979fc3796da4206578664d3616859
David Jimenez <davidjmzom1@gmail.com>
2023-11-17 20:29:48 +0100

    Notificaciones hechas

--- super/urls.py
+++ super/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "super"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("management", views.management, name="management")
+    path("management", views.management, name="management"),
+    path("deleteRes/<int:pk>", views.deleteRes, name="deleteRes"),
+    path("management/<int:pk>", views.oneRestaurant, name="oneRes"),
+    path("management/notificate/<int:pk>", views.notification, name="notification")
 ]
--- super/views.py
+++ super/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from infouser.models import Notification
-from login.models import Restaurant
+from login.models import Restaurant, User
 
 
 # Create your views here.
@@ -19,3 +19,28 @@ def management(request):
     restaurants = Restaurant.objects.all()
     return render(request, "super/management.html",
                   {"restaurants": restaurants})
+
+
+def deleteRes(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    restaurant.delete()
+    return HttpResponseRedirect(reverse("super:management"))
+
+
+def oneRestaurant(request, pk):
+    restaurant = Restaurant.objects.get(pk=pk)
+    print(restaurant.rest_name)
+    return render(request, "super/oneRestaurant.html",
+                  {"restaurant": restaurant})
+
+
+def notification(request, pk):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        emissor = User.objects.get(pk=request.user.pk)
+        receiver = User.objects.get(pk=pk)
+        notif = Notification(emissor=emissor, receiver=receiver, description=description, title=title)
+        notif.save()
+        return HttpResponseRedirect(reverse("super:management"))
+    return render(request, "super/notification.html")
