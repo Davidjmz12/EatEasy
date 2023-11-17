16c4120464378065375fac28ad0ce57ae2f828ab
DavidTizne <davidtizne@gmail.com>
2023-11-17 15:33:11 +0100

    Before merging.

--- EatEasy/settings.py
+++ EatEasy/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -132,5 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
